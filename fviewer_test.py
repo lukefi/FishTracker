d16f59904d2f58f6a7e7f2c4056dcd5897e16cd0
Uimonen Mikael <mikael.uimonen@vtt.fi>
2020-06-18 09:51:58 +0300

    Test on replicating Fisher menu screen.

--- fviewer_test.py
+++ fviewer_test.py
@@ -4,12 +4,15 @@ import sys, os, cv2
 _MAIN_DIRECTORY = os.getcwd()
 sys.path.append(os.path.join(_MAIN_DIRECTORY, "UI"))
 
-import UI_handler as ui
-import file_handler as fh
+import file_handler
 import UI_utils
-import FViewer
+from iconsLauncher import *     # UI/iconsLauncher
 
-
+from FWelcomeInfo import *      # UI/FWelcomeInfo
+from FViewer import *           # UI/FViewer
+import fileMainMenu             # UI/fileMainMenu
+import editMainMenu             # UI/editMainMenu
+import helpMainMenu             # UI/helpMainMenu
 
 class FViewerWidget(QtWidgets.QWidget):
     def __init__(self, parent=None):
@@ -19,9 +22,51 @@ class FViewerWidget(QtWidgets.QWidget):
         self.layout.addWidget(self.viewer)
         self.setLayout(self.layout)
 
+class FTestContainer(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.initUI()
+        return
+        
+        
+
+    def initUI(self):
+        self._MAIN_CONTAINER = self
+        self._CONFIG = file_handler.loadJSON("config.json")
+        self.setWindowIcon(QtGui.QIcon(FGetIcon(self._CONFIG["icon"], OS = sys.platform)))
+        self.setWindowTitle(self._CONFIG["windowTitle"])
+
+        self.FMainMenu_init()
+        self.showNormal()
+        self.width = self._CONFIG["initWidth"]
+        self.height = self._CONFIG["initHeight"]
+        self.left = self._CONFIG["initLeft"]
+        self.top = self._CONFIG["initTop"]
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.FWelcomeInfo = FWelcomeInfo(self)
+        self.FCentralScreen = FViewer(self)
+        self.setCentralWidget(self.FCentralScreen)
+        #self.setCentralWidget(self.FWelcomeInfo)
+
+        
+    def FMainMenu_init(self):
+        self.mainMenu = self.menuBar()
+        fileMainMenu.FFileMenu_init(self)
+        editMainMenu.FEditMenu_init(self)
+        helpMainMenu.FHelpMenu_init(self)
+        self.FStatusBar = QtWidgets.QStatusBar()
+        self.FStatusBarFrameNumber = QtWidgets.QLabel()
+        self.FStatusBarFrameNumber.setText("No File Loaded")
+        self.FStatusBarMousePos = QtWidgets.QLabel()
+        self.FStatusBar.addPermanentWidget(self.FStatusBarMousePos)
+        self.FStatusBar.addPermanentWidget(self.FStatusBarFrameNumber)
+        self.setStatusBar(self.FStatusBar)
+        return
+
 def run():
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = ui.FMainContainer()
+    MainWindow = FTestContainer()
     app.exec_()
     return
 

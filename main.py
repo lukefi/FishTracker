8b866f3457d1e9d887003f470a449f12db936f2c
Mikael Uimonen <mikael.uimonen@aalto.fi>
2021-10-03 19:12:14 +0300

    Added multiprocessing.freeze_support() required by PyInstaller.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from ui_manager import launch_ui
+import multiprocessing
 
 if __name__ == "__main__":
+    # Freezing is required by PyInstaller
+    multiprocessing.freeze_support()
     launch_ui()
